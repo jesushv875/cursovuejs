acee86bce0f7f1676c399aa1df1d432aa36a057a
unknown <sam@google.com>
2020-07-31 22:21:34 -0500

    VIDEO NUM7 del curso, componentes

---
+++ saludos.js
@@ -0,0 +1,14 @@
+Vue.component('saludo',{
+    template: /*html*/
+    `
+    <div>
+    <h1>{{saludo}}</h1>
+    <h3>xdxdxdxd</h3>
+    </div>
+    `,
+    data(){
+        return{
+            saludo: 'hola desdeee vue'
+        }
+    }
+});
