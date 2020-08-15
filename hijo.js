cbcb6423043f3e3d0a81fabdf357cb69e40fca4e
unknown <sam@google.com>
2020-08-14 23:15:00 -0500

    Curso Vuejs video8 comunicacion entre componentes

---
+++ hijo.js
@@ -0,0 +1,9 @@
+Vue.component('hijo',{
+    template: /*html*/
+    `
+   <div class="py-5 bg-success text-white">
+   <h4>componente hijo: {{numero}}</h4> 
+   </div>
+    `,
+    props:['numero']
+})
